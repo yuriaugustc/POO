52b5237f0c99adc7fb0af5f6e0995f23307c70eb
Yuri Augusto da Costa <yuri.costa@sedoc.local>
2021-09-28 12:24:10 -0300

    changes

---
+++ Workspace/src/TrabalhoPOOI/OutrosFuncionarios.java
@@ -0,0 +1,8 @@
+package TrabalhoPOOI;
+
+public class OutrosFuncionarios extends Funcionario{
+
+    public OutrosFuncionarios(String nome, String data_nascimento, byte sexo, int CPF, int RG, char estado_civil) {
+        super(nome, data_nascimento, sexo, CPF, RG, estado_civil);
+    }
+}
